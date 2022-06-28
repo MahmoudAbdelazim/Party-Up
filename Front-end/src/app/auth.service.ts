605851b63444fa5b92b7f233f7b0f9538e590538
Kamel_Mohamed <kemoyar@gmail.com>
2022-06-28 21:42:31 +0200

    Signup API connection from front to back -there is some additional implementation in the backEnd-

--- Front-end/src/app/auth.service.ts
+++ Front-end/src/app/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import {RegisterRequestPayload} from "./register/register-request.payload";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class AuthService {
 
   constructor(private _httpClient:HttpClient) { }
 
-  register(){
-    this._httpClient.post('', []);
+  register(registerPayload : RegisterRequestPayload) : Observable<any>{
+    return this._httpClient.post('http://localhost:8080/api/auth/signup', registerPayload, {responseType : 'text'});
   }
 }
--- Front-end/src/app/register/register.component.ts
+++ Front-end/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router'
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {RegisterRequestPayload} from "./register-request.payload";
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-register',
@@ -9,7 +11,19 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  registerPayload : RegisterRequestPayload;
+
+  constructor(private router:Router, private authService : AuthService) {
+      this.registerPayload = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        discordTag: "",
+        dataOfBirth: "",
+        country: ""
+      };
+  }
 
   registerForm = new FormGroup({
     firstName: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.pattern('^[a-zA-Z]{3,20}$'), Validators.maxLength(20)]), //inside the constructor is like a place holder
@@ -27,6 +41,17 @@ export class RegisterComponent implements OnInit {
 
 
   submitRegisterForm(registerForm:FormGroup){
+      this.registerPayload.firstName = this.registerForm.get('firstName')?.value;
+      this.registerPayload.lastName = this.registerForm.get('lastName')?.value;
+      this.registerPayload.email = this.registerForm.get('email')?.value;
+      this.registerPayload.password = this.registerForm.get('password')?.value;
+      this.registerPayload.discordTag = this.registerForm.get('discordTag')?.value;
+      this.registerPayload.dataOfBirth = this.registerForm.get('dateOfBirth')?.value;
+      this.registerPayload.country = this.registerForm.get('country')?.value;
+
+      this.authService.register(this.registerPayload).subscribe(data =>{
+        console.log("sign up successfully")
+      });
       this.router.navigate(['/personalityTest']);
   }
 }
