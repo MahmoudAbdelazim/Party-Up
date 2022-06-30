7ae959bb651c9b2a419a1806863bd2bc91224249
Kamel_Mohamed <kemoyar@gmail.com>
2022-06-30 02:00:50 +0200

    Login Api connection

--- Front-end/src/app/auth.service.ts
+++ Front-end/src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import {RegisterRequestPayload} from "./register/register-request.payload";
+import {LoginRequestPayload} from "./login/login-request.payload";
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,8 @@ export class AuthService {
 
   register(registerPayload : RegisterRequestPayload) : Observable<any>{
     return this._httpClient.post('http://localhost:8080/api/auth/signup', registerPayload, {responseType : 'text'});
+  }
+  signIn(loginPayload : LoginRequestPayload) : Observable<any>{
+    return this._httpClient.post('http://localhost:8080/api/auth/signin', loginPayload, {responseType : 'text'});
   }
 }
---
+++ Front-end/src/app/login/login-request.payload.ts
@@ -0,0 +1,4 @@
+export interface LoginRequestPayload{
+  usernameOrEmail: string
+  password: string
+}
