8d09ee8894c31a298216db35e555eaa23bdc2a56
Kamel_Mohamed <kemoyar@gmail.com>
2022-07-01 20:17:03 +0200

    Personality Test 'front-end' but lack of pretty design :(

--- Front-end/src/app/personality-test/personality-test.component.ts
+++ Front-end/src/app/personality-test/personality-test.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-personality-test',
@@ -8,6 +10,7 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class PersonalityTestComponent implements OnInit {
 
+/*
   playerPersonalityTest = new FormGroup({
     q1: new FormControl(null , [Validators.required]),
     q2: new FormControl(null , [Validators.required]),
@@ -37,11 +40,86 @@ export class PersonalityTestComponent implements OnInit {
     q26: new FormControl(null , [Validators.required]),
     q27: new FormControl(null , [Validators.required])
 })
+*/
+  step: any = 1;
+  forTheSpan: boolean = false;
+  submitted: any = false;
+  multistep = new FormGroup({
+    Page1: new FormGroup({
+      q1: new FormControl(null , [Validators.required]),
+      q2: new FormControl(null , [Validators.required]),
+      q3: new FormControl(null , [Validators.required]),
+      q4: new FormControl(null , [Validators.required]),
+      q5: new FormControl(null , [Validators.required]),
+      q6: new FormControl(null , [Validators.required]),
+      q7: new FormControl(null , [Validators.required]),
+      q8: new FormControl(null , [Validators.required]),
+      q9: new FormControl(null , [Validators.required]),
+    }),
+    Page2: new FormGroup({
+      q10: new FormControl(null , [Validators.required]),
+      q11: new FormControl(null , [Validators.required]),
+      q12: new FormControl(null , [Validators.required]),
+      q13: new FormControl(null , [Validators.required]),
+      q14: new FormControl(null , [Validators.required]),
+      q15: new FormControl(null , [Validators.required]),
+      q16: new FormControl(null , [Validators.required]),
+      q17: new FormControl(null , [Validators.required]),
+      q18: new FormControl(null , [Validators.required]),
+    }),
+    Page3: new FormGroup({
+      q19: new FormControl(null , [Validators.required]),
+      q20: new FormControl(null , [Validators.required]),
+      q21: new FormControl(null , [Validators.required]),
+      q22: new FormControl(null , [Validators.required]),
+      q23: new FormControl(null , [Validators.required]),
+      q24: new FormControl(null , [Validators.required]),
+      q25: new FormControl(null , [Validators.required]),
+      q26: new FormControl(null , [Validators.required]),
+      q27: new FormControl(null , [Validators.required])
+    })
+  })
+  constructor(private route: Router) { }
 
-  constructor() { }
-
-  ngOnInit(){
+  ngOnInit(): void {
   }
+
+  get Page1() {
+    // @ts-ignore
+    return this.multistep.controls['Page1']['controls'];
+  }
+
+  get Page2() {
+    // @ts-ignore
+    return this.multistep.controls['Page2']['controls'];
+  }
+
+  submit() {
+    this.submitted = true;
+    if(this.multistep.controls['Page1'].invalid && this.step == 1) {
+      this.forTheSpan = true;
+      return;
+    }
+    if(this.multistep.controls['Page2'].invalid && this.step == 2) {
+      this.forTheSpan = true;
+      return;
+    }
+    if(this.multistep.controls['Page3'].invalid && this.step == 3) {
+      this.forTheSpan = true;
+      return;
+    }
+    this.forTheSpan = false;
+    this.step = this.step + 1;
+    if(this.step == 4) {
+      this.route.navigate(['/login'])
+    }
+  }
+
+  previous() {
+    this.forTheSpan = false;
+    this.step = this.step - 1;
+  }
+
 
 
 }
