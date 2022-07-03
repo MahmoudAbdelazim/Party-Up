5c97fd2171b8e4ccd987bf5f0a517d17ab25bfe4
Kamel_Mohamed <kemoyar@gmail.com>
2022-07-03 21:53:04 +0200

    edit in personality test Api connection

--- Front-end/src/app/personality-test.service.ts
+++ Front-end/src/app/personality-test.service.ts
@@ -11,7 +11,8 @@ export class PersonalityTestService {
   constructor(private _httpClient:HttpClient) {
   }
 
-  sendPersonalityTestAnswers(ptPayloadAnswers : PersonalityTestRequestPayload[]) : Observable<any>{
-    return this._httpClient.post('http://localhost:8080/api/personalityTest', ptPayloadAnswers , {responseType : 'text'});
+  sendPersonalityTestAnswers(ptPayloadAnswers : PersonalityTestRequestPayload[] , userName : string) : Observable<any>{
+    let url : string = 'http://localhost:8080/api/personalityTest/' + userName;
+    return this._httpClient.post( url , ptPayloadAnswers , {responseType : 'text'});
   }
 }
