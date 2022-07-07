a2b351b5199de050b6ab1407d06f337ac0ce9190
MahmoudAbdelazim <mahmoud2011989@gmail.com>
2022-07-07 19:46:53 +0200

    Update ML KNN

--- ML-Model/ML-Model-API/app.py
+++ ML-Model/ML-Model-API/app.py
@@ -26,61 +26,56 @@ db = pymysql.connect(host="localhost", user=os.getenv('DB_USERNAME'), password=o
 pd.set_option('display.float_format', lambda x: '%.2f' % x)
 
 personality_data = pd.read_sql('SELECT * FROM players_rates_questions', db)
-personality_data = personality_data.pivot_table('rate', 'player_id', 'questionid')
+personality_data = personality_data.pivot_table('rate', 'player_id', 'questionid').reset_index()
 # print(personality_data)
 
 user_games_data = pd.read_sql('SELECT * FROM player_games', db)
-print(user_games_data)
+# print(user_games_data)
 
-# # K-Means Clustering
-# from sklearn.cluster import AgglomerativeClustering
-# def k_means():
-#     kmeans = KMeans(n_clusters=3, random_state=42).fit(personality_data.iloc[:, 1:4])
-#     labels = kmeans.labels_
-#     print(metrics.calinski_harabasz_score(personality_data.iloc[:, 1:28], labels)) # Variance Ratio Criterion
-#     personality_data['cluster'] = labels
-#     print(personality_data)
-# k_means()
+def k_means():
+    kmeans = KMeans(n_clusters=max(2, personality_data.size // 1000), random_state=42).fit(personality_data.iloc[:, 1:28])
+    labels = kmeans.labels_
+    # print(metrics.calinski_harabasz_score(personality_data.iloc[:, 1:28], labels)) # Variance Ratio Criterion
+    personality_data['cluster'] = labels
+    # print(personality_data)
+k_means()
 
-# def get(user_id, game_id):
-    # clusterNum = data[data['ID'] == id]['cluster'].tolist()[0]
-    # cluster = data.loc[data['cluster'] == clusterNum]
-    # distances, neighbors = knn(cluster, id)
-    # sorted_cluster = cluster.iloc[neighbors.tolist()[0]]
-    # cluster = sorted_cluster.copy()
-    # sorted_cluster['distance'] = distances.tolist()[0]
-    # ids = sorted_cluster['ID'].tolist()
-    # df = sorted_cluster.iloc[[0]]
-    # game_users = users_data.loc[users_data['Game'] == game]
-    # for i in ids:
-    #     df2 = game_users.loc[game_users['ID'] == i]
-    #     if len(df2):
-    #         df = df.append(sorted_cluster.loc[sorted_cluster['ID'] == i])
-    # return df.loc[:, ['ID', 'cluster', 'distance']]
+def knn(cluster, id):
+    nbrs = NearestNeighbors(n_neighbors=len(cluster), algorithm='ball_tree').fit(cluster.iloc[:, 1:28])
+    test = cluster.loc[cluster['player_id'] == id].drop(columns=['player_id', 'cluster'])
+    return nbrs.kneighbors(test, return_distance=True)
 
-# KNN
-# def knn(cluster, id):
-#     nbrs = NearestNeighbors(n_neighbors=len(cluster), algorithm='ball_tree').fit(cluster.iloc[:, 1:28])
-#     test = cluster.loc[cluster['ID'] == id].drop(columns=['ID', 'cluster'])
-#     return nbrs.kneighbors(test, return_distance=True)
+def getDataFrame(player_id, game_id):
+    clusterNum = personality_data[personality_data['player_id'] == player_id]['cluster'].tolist()[0]
+    cluster = personality_data.loc[personality_data['cluster'] == clusterNum]
+    distances, neighbors = knn(cluster, player_id)
+    sorted_cluster = cluster.iloc[neighbors.tolist()[0]]
+    sorted_cluster['distance'] = distances.tolist()[0]
+    ids = sorted_cluster['player_id'].tolist()
+    df = sorted_cluster.iloc[[0]]
+    game_users = user_games_data.loc[user_games_data['game_id'] == game_id]
+    for i in ids:
+        df2 = game_users.loc[game_users['player_id'] == i]
+        if len(df2):
+            df = df.append(sorted_cluster.loc[sorted_cluster['player_id'] == i])
+    return df.loc[:, ['player_id']]
 
+def get(player_id, game_id):
+    idsList = getDataFrame(player_id, game_id)['player_id'].tolist()
+    res = []
+    for id in idsList:
+        res.append({'id': id})
+    return res
 
-@app.route('/<int:user_id>/<int:game_id>')
-def hello_world(user_id, game_id):
+print(get(1, 1))
+
+@app.route('/<player_id>/<int:game_id>')
+def hello_world(player_id, game_id):
+    player_id = int(player_id)
+    game_id = int(game_id)
+    player_ids = get(player_id, game_id)
     response = app.response_class(
-        response=json.dumps(
-            [
-                {'id': 2},
-                {'id': 3},
-                {'id': 4},
-                {'id': 5},
-                {'id': 6},
-                {'id': 7},
-                {'id': 8},
-                {'id': 9},
-                {'id': 10}
-            ]
-        ),
+        response=json.dumps(player_ids),
         status=200,
         mimetype='application/json'
     )
