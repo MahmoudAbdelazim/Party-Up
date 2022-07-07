55b46055c6cabfb3d906a85f887dd7e21ff768bb
MahmoudAbdelazim <mahmoud2011989@gmail.com>
2022-07-07 22:24:49 +0200

    Update ML Model to search in other clusters

--- ML-Model/ML-Model-API/app.py
+++ ML-Model/ML-Model-API/app.py
@@ -10,8 +10,6 @@ app = Flask(__name__)
 
 db = pymysql.connect(host="localhost", user=os.getenv('DB_USERNAME'), password=os.getenv('DB_PASSWORD'), database="partyup")
 
-pd.set_option('display.float_format', lambda x: '%.2f' % x)
-
 personality_data = pd.read_sql('SELECT * FROM players_rates_questions', db)
 personality_data = personality_data.pivot_table('rate', 'player_id', 'questionid').reset_index()
 # print(personality_data)
@@ -19,39 +17,62 @@ personality_data = personality_data.pivot_table('rate', 'player_id', 'questionid
 user_games_data = pd.read_sql('SELECT * FROM player_games', db)
 # print(user_games_data)
 
+
 def k_means():
-    kmeans = KMeans(n_clusters=max(2, personality_data.size // 1000), random_state=42).fit(personality_data.iloc[:, 1:28])
+    num_clusters = max(2, len(personality_data) // 1000)
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42).fit(personality_data.iloc[:, 1:28])
     labels = kmeans.labels_
     # print(metrics.calinski_harabasz_score(personality_data.iloc[:, 1:28], labels)) # Variance Ratio Criterion
     personality_data['cluster'] = labels
-    # print(personality_data)
+    return num_clusters
 
-def knn(cluster, id):
+def knn(cluster, player_dataframe):
     nbrs = NearestNeighbors(n_neighbors=len(cluster), algorithm='ball_tree').fit(cluster.iloc[:, 1:28])
-    test = cluster.loc[cluster['player_id'] == id].drop(columns=['player_id', 'cluster'])
-    return nbrs.kneighbors(test, return_distance=True)
+    return nbrs.kneighbors(player_dataframe, return_distance=True)
 
-def getDataFrame(player_id, game_id):
-    clusterNum = personality_data[personality_data['player_id'] == player_id]['cluster'].tolist()[0]
-    cluster = personality_data.loc[personality_data['cluster'] == clusterNum]
-    distances, neighbors = knn(cluster, player_id)
+def addDataFromCluster(game_id, cluster_num, ids_list, size, player_dataframe):
+    cluster = personality_data.loc[personality_data['cluster'] == cluster_num]
+    distances, neighbors = knn(cluster, player_dataframe)
     sorted_cluster = cluster.iloc[neighbors.tolist()[0]]
     sorted_cluster['distance'] = distances.tolist()[0]
     ids = sorted_cluster['player_id'].tolist()
-    df = sorted_cluster.iloc[[0]]
     game_users = user_games_data.loc[user_games_data['game_id'] == game_id]
     for i in ids:
         df2 = game_users.loc[game_users['player_id'] == i]
         if len(df2):
-            df = df.append(sorted_cluster.loc[sorted_cluster['player_id'] == i])
-    return df.loc[:, ['player_id']]
+            ids_list.append(sorted_cluster.loc[sorted_cluster['player_id'] == i]['player_id'].tolist()[0])
+            if len(ids_list) == size:
+                return ids_list
+    return ids_list
+
+def getDataFrame(player_id, game_id, size, num_clusters):
+    cluster_num = personality_data[personality_data['player_id'] == player_id]['cluster'].tolist()[0]
+    cluster = personality_data.loc[personality_data['cluster'] == cluster_num]
+    player_dataframe = cluster.loc[cluster['player_id'] == player_id].drop(columns=['player_id', 'cluster'])
+    distances, neighbors = knn(cluster, player_dataframe)
+    sorted_cluster = cluster.iloc[neighbors.tolist()[0]]
+    sorted_cluster['distance'] = distances.tolist()[0]
+    ids_list = []
+    upper_cluster = cluster_num
+    lower_cluster = cluster_num - 1
+    while len(ids_list) < size:
+        if upper_cluster < num_clusters:
+            ids_list = addDataFromCluster(game_id, upper_cluster, ids_list, size, player_dataframe)
+        if len(ids_list) < size and lower_cluster >= 0:
+            ids_list = addDataFromCluster(game_id, lower_cluster, ids_list, size, player_dataframe)
+        if upper_cluster >= num_clusters and lower_cluster < 0:
+            break
+        upper_cluster += 1
+        lower_cluster -= 1
+    return ids_list
 
 def get(player_id, game_id):
-    k_means()
-    idsList = getDataFrame(player_id, game_id)['player_id'].tolist()
+    num_clusters = k_means()
+    idsList = getDataFrame(player_id, game_id, 20, num_clusters)
     res = []
     for id in idsList:
-        res.append({'id': id})
+        if (player_id != id):
+            res.append({'id': id})
     return res
 
 print(get(1, 1))
